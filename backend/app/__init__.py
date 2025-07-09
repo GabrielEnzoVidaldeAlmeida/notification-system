7d9ae5a6216d0068780f921093cae0f9c538f309
Gabriel Enzo <111479378+GabrielEnzoVidaldeAlmeida@users.noreply.github.com>
2025-07-08 22:12:01 -0300

    fix: adjust run configuration to support Flask-SocketIO

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -8,7 +8,9 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 
-load_dotenv('../.env')
+# load_dotenv('../.env')
+load_dotenv('.env')
+
 
 db = SQLAlchemy()
 migrate = Migrate()
--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,11 @@
+# run.py
+
+import eventlet
+eventlet.monkey_patch()  # ✅ Isso deve estar no topo, antes de tudo!
+
 from app import create_app, socketio
 
 app = create_app()
 
 if __name__ == '__main__':
-    socketio.run(app) 
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True)
